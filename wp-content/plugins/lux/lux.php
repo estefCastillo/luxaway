425f1cd30c62ec8e42a4e0d3ea5319112dc7f8ef
estefCastillo <rocsamcastillo@gmail.com>
2024-10-23 22:02:21 +0200

    pluginYA

--- wp-content/plugins/lux/lux.php
+++ wp-content/plugins/lux/lux.php
@@ -24,12 +24,35 @@
  
 
  function lux_instala(){
+    global $wpdb;
+    $table_name = $wpdb->prefix . "destinos"; 
    
+    $sql = "CREATE TABLE $table_name (
+        id INT(11) NOT NULL AUTO_INCREMENT,
+        destino TEXT NOT NULL,
+        PRIMARY KEY (id)
+    );";
+    
+    
+    $wpdb->query($sql);
+
+    $sql = "INSERT INTO $table_name (destino) VALUES 
+        ('Santiago, Cuba'),
+        ('Tokio, Japón'),
+        ('San Juan, Puerto Rico'),
+        ('Nueva York, EE.UU.'),
+        ('Bali, Indonesia');";
+    
+    $wpdb->query($sql);
  }
  
 
  function lux_desinstala(){
-    
+    global $wpdb;
+    $table_name = $wpdb->prefix . "destinos";
+
+    $sql = "DROP TABLE IF EXISTS $table_name;";
+    $wpdb->query($sql);
  }
  
  
@@ -39,17 +62,26 @@
 
  function lux_panel(){
      include('template/panel.html');
-     echo "<h2>Página de configuración de Lux</h2>";
+     global $wpdb;
+     $table_name = $wpdb->prefix . "destinos";
+
+
+     if (isset($_POST['lux'])) {
+         $destino = sanitize_text_field($_POST['lux']); 
+         
+         $sql = "INSERT INTO $table_name (destino) VALUES ('$destino');";
+         $wpdb->query($sql);
  }
- 
+}
+
 
  function lux_add_menu(){
     if (function_exists('add_menu_page')) {
         add_menu_page('Lux', 'Lux', 'manage_options', 'lux', 'lux_panel', '', 6);
     }
 }
- 
- add_action('admin_menu', 'lux_add_menu');
+
+    add_action('admin_menu', 'lux_add_menu');
  
  ?>
  
