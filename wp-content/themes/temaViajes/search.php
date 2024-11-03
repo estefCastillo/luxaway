fbfab7940b6c243c296f2bf4d192c5dcbe7b200c
estefCastillo <rocsamcastillo@gmail.com>
2024-11-03 17:24:48 +0100

    retoques

--- wp-content/themes/temaViajes/search.php
+++ wp-content/themes/temaViajes/search.php
@@ -3,8 +3,8 @@
  <h2>Resultados de la búsqueda:</h2>
  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
  <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
- <!-- <small>Publicado el <?php the_time('j/m/Y') ?> por <?php
-the_author_posts_link() ?> </small> -->
+ <small>Publicado el <?php the_time('j/m/Y') ?> por <?php
+the_author_posts_link() ?> </small>
  <div class="thumbnail">
  <?php
  if ( has_post_thumbnail() ) {
